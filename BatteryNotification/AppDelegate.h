5dd0207fa7c43748dc764de8096480515667796e
Ryan Huynh <ryanhuynh@ryans-macbook-air.local>
2016-05-31 18:50:22 -0700

    Initial Commit

---
+++ BatteryNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BatteryNotification
+//
+//  Created by Ryan Huynh on 5/31/16.
+//  Copyright © 2016 Info Way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BatteryNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatteryNotification
+//
+//  Created by Ryan Huynh on 5/31/16.
+//  Copyright © 2016 Info Way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
